09027428112198b8191b4dc4768c41a97cc6e74f
kazagan <steffen.paige@outlook.com>
2022-11-10 00:01:14 -0600

    char comparisons wip

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Repository where I plan to write sorting algorithms in C to help better understand them.
 
+Int Comparisons are working now for equality, allowing me to assert on the arrays, so I can have a sort of unit test for the sorting library
+
+Writing the string comparisons is working, except when figuring out how to incremement the char array but the correct number of characters after checking a work.
+Need to figure out a solution.
+
+current idea centers around possibly returning -1 when value is not equal, but size of word when they are, 
+but am unsure how this would apply to future comparisons, when writing the actual sorting algorithms.
--- start.c
+++ start.c
@@ -3,18 +3,28 @@
 
 void assert_array(size_t type_size, void *expected, size_t expected_size, void *actual, size_t actual_size, int (*comparison)(const void *, const void*));
 int int_equality(const void * first, const void * second);
+int str_equality(const void * expected,const void * actual);
 
 int main() {
     
     // Initialize some arrays of differing types.
-
     int unordered_int[10] = { 20, 30, 5, 9, 60, 55, 2, 9, 89, 38 };
     int ordered_int[10] = { 2, 5, 9, 9, 20, 30, 38, 55, 60, 89 };
+
+    char *unordered_str[10] = { "MARY", "PATRICIA", "LINDA", "BARBARA", "ELIZABETH", "JENNIFER", "MARIA", "SUSAN", "MARGARET", "DOROTHY" };
+    char *ordered_str[10] = { "BARBARA", "DOROTHY", "ELIZABETH", "JENNIFER", "LINDA", "MARGARET", "MARIA", "MARY", "PATRICIA", "SUSAN" };
+    char *test_str[10] = { "BARBARA", "DOROTHY", "ELIZABETH", "JENNIFER", "LINDA", "MARGARET", "MARIA", "MARY", "PATRICIA", "SUSAN" };
     // This is where I would call my sorting Algorithm... IF I HAD ONE!!
     
     // Assert equals.
-    assert_array(sizeof(int), ordered_int, 10, unordered_int, 10, int_equality);
-    
+    /* assert_array(sizeof(int), ordered_int, 10, unordered_int, 10, int_equality); */
+    /* char *x = "Barbara"; */
+    /* char *y = "Barbara"; */
+    /* void *a = &x; */
+    /* void *b = &y; */
+
+    /* str_equality(a, b); */
+    assert_array(sizeof(char), ordered_str, 10, test_str, 10, str_equality);
     return 0;
 }
 
@@ -26,17 +36,37 @@ int int_equality(const void * expected, const void * actual) {
     return 1;
 }
 
+int str_equality(const void * expected,const void * actual) {
+    int i = 0;
+    char *x = *(char**)expected;
+    char *y = *(char**)actual;
+    while(x[i] != '\0') {
+        if(x[i] != y[i]) {
+            fprintf(stderr, "%s differs from %s at '%c' not equal to '%c'\n", x, y, x[i], y[i]);
+            return 0;
+        }
+        i++;
+    }
+    expected += i;
+    actual += i;
+    return 1;
+}
+
 void assert_array(size_t type_size, void *expected, size_t expected_size, void *actual, size_t actual_size, int (*comparison)(const void *, const void*)) {
     if(expected_size != actual_size) {
         fprintf(stderr, "Error: actual size %ld does not equal expected size %ld\n", actual_size, expected_size);
         exit(-1);
     }
-    for(int i = 0; i < expected_size; i++) {
+    for(int i = 0; i < 1; i++) {
+        printf("expected %p actual %p\n", expected, actual);
         if(comparison(expected, actual) != 1) {
             fprintf(stderr, "arrays differ at element %d\n", i+1);
             exit(-1);
         }
+
+        printf("expected %p actual %p\n", expected, actual);
         expected += type_size;
         actual += type_size;
+        printf("expected %p actual %p\n", expected, actual);
     }
 }
